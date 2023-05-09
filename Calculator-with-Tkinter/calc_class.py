0a7a547c27a9c4d9d6d1741bf4efbca19833fb29
Kelvyn dos Santos <kelvynsantos10@outlook.com>
2023-05-09 13:25:30 -0300

    confim

--- Calculator-with-Tkinter/calc_class.py
+++ Calculator-with-Tkinter/calc_class.py
@@ -1,6 +1,7 @@
+import re
 import tkinter as tk
 from typing import List
-
+import math
 class Calculator:
     """teste"""
     def __init__(self, root: tk.Tk, label: tk.Label, display: tk.Entry,buttons: List[List[tk.Button]]):
@@ -21,14 +22,45 @@ class Calculator:
                 if button_text == 'C': 
                     button.bind('<Button-1>', self.clear)
                 if button_text == '0123456789.+-/*^()':
-                    button.bind('<Button-2', self.add_text_to_display)
+                    button.bind('<Button-1', self.add_text_to_display)
+                if button_text == '=':
+                    button.bind('Button-1', self.calculate)
                 
                 
     def _config_display(self):
+        # self._fix_text(self.display.get())
         ...
         
+    def _fix_text(self,text):
+        text = re.sub(r'[^\d\.\/\*\-\+\(\)\^e]',r'',text,0)
+        text = re.sub(r'([\.\+\/\-\*\^])\1+',r'\1',text,0)
+        text = re.sub(r'\*?\(\)','',text)
+        
+        return text        
     def clear(self, event=None):
         self.display.delete(0,'end')
         
     def add_text_to_display(self, event=None):
         self.display.insert('end', event.widget['text'])
+        
+    def calculate(self, event=None):
+        fixed_text = self._fix_text(self.display.ge())
+        equations = self._get_equations(fixed_text)
+        
+        try:
+            if len(equations) == 1:
+                result = eval(self._fix_text(equations[0]))
+            else:
+                result = eval(self._fix_text(equations[0]))
+                for equation in equations[1:]:
+                    result = math.pow(result, eval(self._fix_text(equations)))
+            self.display.delete(0, 'end')
+            self.display.insert('end',result)
+            self.label.config(text=f'{fixed_text} = {result}')
+            
+        except OverflowError:
+            self.label.config(text= 'Não consegui realizar essa conta, sorry!')
+        except Exception:
+            self.label.config(text = 'Conta inválida!')
+    def _get_equations(self, text):
+        return re.split(r'\^',text,0)
--- Calculator-with-Tkinter/calc_factories.py
+++ Calculator-with-Tkinter/calc_factories.py
@@ -39,17 +39,17 @@ def make_buttons(root) -> List[List[tk.Button]]:
     for row, row_value in enumerate(button_texts, start=2):
         button_row = []
         for col_index, col_value in enumerate(row_value):
-            btn = tk.Button(root, text= col_value)
+            btn = tk.Button(root , text= col_value)
             btn.grid(row=row, column=col_index, stick='news',padx=5,pady=5)
             btn.config(font=('Helvetica',15, 'normal'),pady=40,width=1,background='#f1f2f3', bd=0,cursor='hand2', highlightthickness=0,
                        highlightcolor='#ccc', activebackground='#ccc', highlightbackground='#ccc')
             if col_value in (')', '('):
                 btn.place(width=0.5)
-                btn.grid(row=row, column=col_index, stick='news',padx=(0,2),pady=5)
+                btn.grid(row=row, column=col_index, stick='news',padx=2,pady=5)
 
             else:
-                btn.config(width=1)
-                
+                if col_value == '=' :
+                    btn.config(width=1)
             button_row.append(btn)
         buttons.append(button_row)
     return buttons
